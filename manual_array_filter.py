abb0dc12ebd4a23d8a90bf5f0b1dee257738ba40
Pyt-git <weicky0804@gmail.com>
2026-05-10 11:00:53 -0400

    Create manual_array_filter.py

---
+++ manual_array_filter.py
@@ -0,0 +1,10 @@
+def array_filter(array): 
+    i = 0 
+
+    while i < len(array): 
+        if array[i]["String"] == condition: 
+            array.pop[i]
+        else: 
+             i += 1
+
+    return array
